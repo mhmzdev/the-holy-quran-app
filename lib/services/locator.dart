0d6182d5138b9807834f12be69f84d73304f093c
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2025-02-21 17:45:45 +0500

    service locator added

---
+++ lib/services/locator.dart
@@ -0,0 +1,37 @@
+import 'package:al_quran/blocs/chapter/bloc.dart';
+import 'package:al_quran/blocs/juz/cubit.dart';
+import 'package:al_quran_api/al_quran_api.dart';
+import 'package:al_quran_repo/al_quran_repo.dart';
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> initServiceLocator() async {
+  // Providers
+  sl.registerSingleton<AlQuranDataProviderImpl>(AlQuranDataProviderImpl());
+
+  // Repos
+  sl.registerSingleton<ChapterRepo>(
+    ChapterRepo(
+      provider: sl(),
+    ),
+  ); 
+  sl.registerSingleton<JuzRepo>(
+    JuzRepo(
+      provider: sl(),
+    ),
+  );
+
+  // Blocs
+  sl.registerSingleton<ChapterBloc>(
+    ChapterBloc(
+      repo: sl(),
+    ),
+  );
+
+  sl.registerSingleton<JuzCubit>(
+    JuzCubit(
+      repo: sl(),
+    ),
+  );
+}
