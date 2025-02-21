0cbeb17193c6e950f2461a3d46cb3f29bcfdcccf
Muhammad Hamza <hamza.6.shakeel@gmail.com>
2025-02-21 09:04:32 +0500

    [hygen] updated and added test extension

--- _templates/screen/new/prompt.js
+++ _templates/screen/new/prompt.js
@@ -3,22 +3,6 @@
 //
 module.exports = [
   {
-    type: 'confirm',
-    name: 'formData',
-    default: false,
-    message: 'Do you want Dev data for forms with new screen ?',
-  },
-  {
-    type: 'input',
-    name: 'formKeys',
-    message: 'Write the name of form keys in comma separated values.',
-    result: (data) => {
-      if (!data) return [];
-      const parsed = data.split(',');
-      return parsed;
-    },
-  },
-  {
     type: 'input',
     name: 'widgets',
     message: 'Write the name of widgets in comma separated values.',
